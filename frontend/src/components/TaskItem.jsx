90444eef7c8cd85fbcc22d3ab422e7ce69c83d6e
garvv29 <garv.choure2023@ssipmt.com>
2026-06-08 02:32:55 +0530

    TaskITem

---
+++ frontend/src/components/TaskItem.jsx
@@ -0,0 +1,27 @@
+function taskItem({
+    task,onDelete,onToggle
+}) {
+    return(
+        <li>
+            <span
+                style={{textDecoration:task.completed?"line-through":"none"}}
+                >{task.title}</span>
+
+            <button
+                onClick={()=>
+                    onToggle(task._id || task.id)
+                }
+                >
+                    Toggle
+                </button>
+
+                <button
+                onClick={()=>
+                    onDelete(task._id || task.id)
+                }
+                >Delete</button>
+        </li>
+    );
+}
+
+export default taskItem;
